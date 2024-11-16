a80bab1521e6dfc36c8fe3f9200d64c6a7d1b626
Filip Vidojevic <96ficke@gmail.com>
2024-11-16 11:23:57 +0100

    Obrisan visak koda

--- cas05/6_add_minutes.c
+++ cas05/6_add_minutes.c
@@ -27,32 +27,6 @@
     }\
   } while (0)
 
-void ispisi_dan_u_nedelji(int tm_wday){
-    switch(tm_wday){
-        case 1:
-            printf("Ponedeljak\n");
-            break;
-        case 2:
-            printf("Utorak\n");
-            break;
-        case 3:
-            printf("Sreda\n");
-            break;
-        case 4:
-            printf("Cetvrtak\n");
-            break;
-        case 5:
-            printf("Petak\n");
-            break;
-        case 6:
-            printf("Subota\n");
-            break;
-        case 0:
-            printf("Nedelja\n");
-            break;
-    }
-}
-
 int main(int argc, char** argv) {
     check_error(argc == 2, "./a.out n");
 
