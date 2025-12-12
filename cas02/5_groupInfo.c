604745ac7b4132eae1100c501ec0fd6f957dd502
Filip Vidojevic <96ficke@gmail.com>
2025-12-12 23:35:43 +0100

    Azuriran 5. zadatak

--- cas02/5_groupInfo.c
+++ cas02/5_groupInfo.c
@@ -1,44 +1,39 @@
 /*
-Program treba da ispise informacije o grupi 'groupname',
-pri cemu se informacije dobijaju iz group fajla.
+Ispisati informacije o grupi ciji se naziv zadaje kao
+argument komandne linije.
 
 Poziv programa:
-./groupInfo groupname
+./groupInfo <group_name>
 */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include <grp.h>
 
-#define check_error(cond,userMsg)\
-	do {\
-		if (!(cond)) {\
-			fprintf(stderr,"Greska: %s\n",userMsg);\
-			exit(EXIT_FAILURE);\
-		}\
-	} while (0)
-	
-// ./groupInfo grpName
-int main(int argc, char** argv) {
-	
-	check_error(argc == 2, "args");
-	
-	struct group* groupInfo = getgrnam(argv[1]);
-	check_error(groupInfo != NULL, "...");
-	
-	printf("Group name: %s\n", groupInfo->gr_name);
-	printf("Group password: %s\n", groupInfo->gr_passwd);
-	printf("Group ID: %d\n", (int)groupInfo->gr_gid);
-	for (int i = 0; groupInfo->gr_mem[i] != NULL; i++) {
-			
-			printf("\t%s\n", groupInfo->gr_mem[i]);
-	}
+#define check_error(cond, userMsg)                                                                 \
+    do {                                                                                           \
+        if (!(cond)) {                                                                             \
+            fprintf(stderr, "Greska: %s\n", userMsg);                                              \
+            exit(EXIT_FAILURE);                                                                    \
+        }                                                                                          \
+    } while (0)
 
-	exit(EXIT_SUCCESS);
+int main(int argc, char **argv)
+{
+    check_error(argc == 2, "./a.out <group_name>");
+
+    struct group *groupInfo = getgrnam(argv[1]);
+    check_error(groupInfo != NULL, "getgrnam");
+
+    printf("Group name: %s\n", groupInfo->gr_name);
+    printf("Group password: %s\n", groupInfo->gr_passwd);
+    printf("Group ID: %jd\n", (intmax_t)groupInfo->gr_gid);
+    for (int i = 0; groupInfo->gr_mem[i] != NULL; i++) {
+
+        printf("\t%s\n", groupInfo->gr_mem[i]);
+    }
+
+    exit(EXIT_SUCCESS);
 }
