c24174a7f9cd31b2e43cca8e48cc37560503f173
Filip Vidojevic <96ficke@gmail.com>
2025-12-12 01:45:24 +0100

    Azurirani uvodni primeri za passwd

--- cas02/1_userInfo.c
+++ cas02/1_userInfo.c
@@ -1,69 +1,70 @@
 /*
-Program treba da ispise informacije o korisniku cije je ime 'username',
-pri cemu se informacije dobijaju iz passwd fajla.
+Ispisati informacije o korisniku cije je ime 'username'.
+Ukoliko korisnik ne postoji, ispisati odgovarajuću poruku na
+standardni izlaz.
 
 Poziv programa:
 ./userInfo username
 */
 
+#include <errno.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <fcntl.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdbool.h>
-
-/* Neophodno zaglavlje prilikom rada sa funkcijama 
+/* Neophodno zaglavlje prilikom rada sa funkcijama
  * koje citaju passwd fajl.
  */
 #include <pwd.h>
 
-#define check_error(cond,msg)\
-  do {\
-    if (!(cond)) {\
-      perror(msg);\
-      fprintf(stderr, "File: %s\nFunction: %s\nLine: %d\n", __FILE__, __func__, __LINE__);\
-      exit(EXIT_FAILURE);\
-    }\
-  } while (0)
+#define check_error(cond, msg)                                                                     \
+    do {                                                                                           \
+        if (!(cond)) {                                                                             \
+            perror(msg);                                                                           \
+            fprintf(stderr, "File: %s\nFunction: %s\nLine: %d\n", __FILE__, __func__, __LINE__);   \
+            exit(EXIT_FAILURE);                                                                    \
+        }                                                                                          \
+    } while (0)
 
-static char* osUsage = "./userInfo username";
+int main(int argc, char **argv)
+{
+    check_error(argc == 2, "./a.out username");
 
-/* funkcija ispisuje informacije o korisniku iz passwd fajla */
-bool os_get_user_info(const char* username, FILE* outStream) {
-	
-	/* Citamo informacije iz passwd fajla o korisniku sa
-	 * korisnickim imenom username.
-	 * Funkcija getpwnam vraca pokazivac na staticki alociranu strukturu.
-	 * 
-	 * BITNO: Ovu strukturu nikada ne smemo eksplicitno da oslobadjamo
-	 */ 
-	struct passwd* userInfo = getpwnam(username);
-	/* U slucaju greske ili nepostojanja podataka u passwd fajlu o 
-	 * korisniku sa imenom username funkcija getpwnam vraca NULL
-	 * i postavlja errno na odgovarajucu vrednost 
-	 */
-	if (userInfo == NULL)
-		return false;
-	
-	/* Ispisujemo informacije koje postoje u passwd fajlu o korisniku */
-	fprintf(outStream, "User name: %s\n", userInfo->pw_name);
-	fprintf(outStream, "Numerical user id: %jd\n", (intmax_t)userInfo->pw_uid);
-	fprintf(outStream, "Numerical group id: %jd\n", (intmax_t)userInfo->pw_gid);
-	fprintf(outStream, "Comment: %s\n", userInfo->pw_gecos);
-	fprintf(outStream, "Home directory: %s\n", userInfo->pw_dir);
-	fprintf(outStream, "Shell: %s\n", userInfo->pw_shell);
-	
-	return true;
-}
+    /* Citamo informacije iz passwd fajla o korisniku sa
+     * korisnickim imenom username.
+     * Funkcija getpwnam vraca pokazivac na staticki alociranu strukturu.
+     *
+     * BITNO: Ovu strukturu nikada ne smemo eksplicitno da oslobadjamo.
+     * Vise u man stranama.
+     */
+    errno = 0;
+    struct passwd *userInfo = getpwnam(argv[1]);
+    /* U slucaju greske ili nepostojanja podataka u passwd fajlu o
+     * korisniku sa imenom username funkcija getpwnam vraca NULL
+     * i postavlja errno na odgovarajucu vrednost.
+     * Da bismo razlikovali ta dva slucaja, moramo proveriti errno.
+     */
+    if (userInfo == NULL) {
+        if (errno == 0) {
+            printf("Korisnik sa imenom '%s' ne postoji!\n", argv[1]);
+        } else {
+            /* sada smo sigurni da je greska, pa prosledjujemo 0 (false) makrou check_error */
+            check_error(0, "getpwnam");
+        }
+    } else {
+        /* Ispisujemo informacije koje postoje u passwd fajlu o korisniku */
+        printf("User name: %s\n", userInfo->pw_name);
+        printf("Numerical user id: %jd\n", (intmax_t)userInfo->pw_uid);
+        printf("Numerical group id: %jd\n", (intmax_t)userInfo->pw_gid);
+        printf("Comment: %s\n", userInfo->pw_gecos);
+        printf("Home directory: %s\n", userInfo->pw_dir);
+        printf("Shell: %s\n", userInfo->pw_shell);
+    }
 
-int main(int argc, char** argv) {
-	
-	check_error(argc == 2, osUsage);
-	check_error(os_get_user_info(argv[1], stdout), "get user info failed");
-	
-	exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
--- cas02/2_listUsers.c
+++ cas02/2_listUsers.c
@@ -2,83 +2,69 @@
 Program koji ispisuje informacije o svim korisnicima na sistemu.
 */
 #define _XOPEN_SOURCE 700
+#include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <fcntl.h>
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include <pwd.h>
 
-/* Makro pomocu koga mozemo da prevarimo kompajler da ne prijavljuje greske
- * za neiskoriscene promenljive
+/* Makro pomocu koga mozemo da prevarimo kompajler da ne prijavljuje upozorenja
+ * za neiskoriscene promenljive (gcc 2_listUsers.c -Wall -Wextra).
  */
 #define UNUSED(x) ((void)x)
 
-#define check_error(cond,msg)\
-  do {\
-    if (!(cond)) {\
-      perror(msg);\
-      fprintf(stderr, "File: %s\nFunction: %s\nLine: %d\n", __FILE__, __func__, __LINE__);\
-      exit(EXIT_FAILURE);\
-    }\
-  } while (0)
-	
-static char* osUsage = "./listUsers";
+#define check_error(cond, msg)                                                                     \
+    do {                                                                                           \
+        if (!(cond)) {                                                                             \
+            perror(msg);                                                                           \
+            fprintf(stderr, "File: %s\nFunction: %s\nLine: %d\n", __FILE__, __func__, __LINE__);   \
+            exit(EXIT_FAILURE);                                                                    \
+        }                                                                                          \
+    } while (0)
 
-/* Funkcija stampa sadrzaj passwd strukture za trenutnog korisnika */
-bool os_print_user_info(const struct passwd* userInfo, FILE* outStream){
-	
-	/* stampanje informacija o korisniku */
-	fprintf(outStream, "\n##################################\n");
-	fprintf(outStream, "User name: %s\n", userInfo->pw_name);
-	fprintf(outStream, "Numerical user id: %jd\n", (intmax_t)userInfo->pw_uid);
-	fprintf(outStream, "Numeriacl group id: %jd\n", (intmax_t)userInfo->pw_gid);
-	fprintf(outStream, "Comment: %s\n", userInfo->pw_gecos);
-	fprintf(outStream, "Home directory: %s\n", userInfo->pw_dir);
-	fprintf(outStream, "Shell: %s\n", userInfo->pw_shell);
-	
-	return true;
-}
+int main(int argc, char **argv)
+{
+    check_error(argc == 1, "./a.out");
+    /* Varamo kompajler da ne bi prijavio upozorenje */
+    UNUSED(argv);
 
-/* Funkcija lista passwd fajl */
-void os_list_all_users(FILE* outStream) {
-	
-	struct passwd* currentUser;
-	/* Prilikom citanja sadrzaja passwd fajla neophodno je na pocetku
-	 * premotati ceo fajl na pocetak pozivom funkcije setpwent().
-	 * 
-	 * Nema potrebe obradjivati greske za setpwent jer poziv ne puca.
-	 */
-	setpwent();
-	
-	/* Passwd fajl se iterativno cita liniju po liniju pomocu funkcije
-	 * getpwent().
-	 * 
-	 * Funkcija vraca NULL ako dodje do kraja fajla ili ako se dogodi greska.
-	 * Za proveru gresaka mora da se koristi errno promenljiva.
-	 */ 
-	while((currentUser = getpwent()) != NULL) {
-		
-		/* Stampamo informacije o korisniku */
-		check_error(os_print_user_info(currentUser, outStream), "Printing user failed");
-	}
-	
-	/* Nakon sto se zavrsi upotreba passwd fajl MORA da se pravilno
-	 * zatvori pozivom funkcije endpwent().
-	 */ 
-	endpwent();
-}
+    /* Obavezno inicijalizovati pokazivace! */
+    struct passwd *currentUser = NULL;
 
-int main(int argc, char** argv) {
-	
-	UNUSED(osUsage);
-	
-	os_list_all_users(stdout);
-	
-	exit(EXIT_SUCCESS);
+    /* Prilikom citanja sadrzaja passwd fajla neophodno je na pocetku
+     * premotati ceo fajl na pocetak pozivom funkcije setpwent().
+     *
+     * Nema potrebe obradjivati greske za setpwent jer poziv ne puca.
+     */
+    setpwent();
+
+    /* Passwd fajl se iterativno cita liniju po liniju pomocu funkcije
+     * getpwent().
+     *
+     * Funkcija vraca NULL ako dodje do kraja fajla ili ako se dogodi greska.
+     * Za proveru gresaka mora da se koristi errno promenljiva. Ovde cemo taj deo preskociti.
+     */
+    while ((currentUser = getpwent()) != NULL) {
+
+        /* Stampamo informacije o korisniku */
+        printf("User name: %s\n", currentUser->pw_name);
+        printf("Numerical user id: %jd\n", (intmax_t)currentUser->pw_uid);
+        printf("Numerical group id: %jd\n", (intmax_t)currentUser->pw_gid);
+        printf("Comment: %s\n", currentUser->pw_gecos);
+        printf("Home directory: %s\n", currentUser->pw_dir);
+        printf("Shell: %s\n", currentUser->pw_shell);
+    }
+
+    /* Nakon sto se zavrsi upotreba passwd fajl MORA da se pravilno
+     * zatvori pozivom funkcije endpwent().
+     */
+    endpwent();
+
+    exit(EXIT_SUCCESS);
 }
