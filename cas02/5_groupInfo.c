83fdc52a82237d28bff3fa1376ef58d651fc3652
Filip Vidojevic <96ficke@gmail.com>
2025-12-14 13:32:38 +0100

    Dodato objasnjenje za gr_mem

--- cas02/5_groupInfo.c
+++ cas02/5_groupInfo.c
@@ -31,6 +31,33 @@ int main(int argc, char **argv)
     printf("Group name: %s\n", groupInfo->gr_name);
     printf("Group password: %s\n", groupInfo->gr_passwd);
     printf("Group ID: %jd\n", (intmax_t)groupInfo->gr_gid);
+    /*
+     * Polje gr_mem je niz pokazivača na karaktere (char *), tj. niz stringova.
+     *
+     * - Svaki element gr_mem[i] pokazuje na C-nisku (string) koja je
+     *   terminisana nul-karakterom '\0'.
+     *
+     * - Ceo niz gr_mem je NULL-terminated, što znači da se kraj niza
+     *   označava pokazivačem koji ima vrednost NULL.
+     *
+     * Razlika između ova dva pojma:
+     *
+     * 1) Niska (string) terminisana sa '\0':
+     *    char s[] = "user";
+     *    U memoriji izgleda ovako:
+     *      'u' 's' 'e' 'r' '\0'
+     *    '\0' označava kraj JEDNE niske.
+     *
+     * 2) Niz pokazivača terminisan sa NULL:
+     *    char *arr[] = {"pera", "mika", "laza", NULL};
+     *    U memoriji:
+     *      arr[0] -> 'p' 'e' 'r' 'a' '\0'
+     *      arr[1] -> 'm' 'i' 'k' 'a' '\0'
+     *      arr[2] -> 'l' 'a' 'z' 'a' '\0'
+     *      arr[3] -> NULL   <-- kraj niza
+     *
+     * Zato se kroz gr_mem prolazi petljom dok god pokazivač nije NULL.
+     */
     for (int i = 0; groupInfo->gr_mem[i] != NULL; i++) {
 
         printf("\t%s\n", groupInfo->gr_mem[i]);
