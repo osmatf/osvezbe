01778fc3c133dddff015748686d7c572f8b07860
Filip Vidojevic <96ficke@gmail.com>
2025-12-15 20:16:16 +0100

    Dodat sredjen primer za kopiranje fajla

---
+++ cas04/6_mycp.c
@@ -0,0 +1,66 @@
+/*
+Napisati program koji kopira fajl sa jedne lokacije na drugu.
+
+Poziv programa:
+./os_cp destination_file source_file
+*/
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <errno.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define check_error(cond, msg)                                                                     \
+    do {                                                                                           \
+        if (!(cond)) {                                                                             \
+            perror(msg);                                                                           \
+            fprintf(stderr, "File: %s\nFunction: %s\nLine: %d\n", __FILE__, __func__, __LINE__);   \
+            exit(EXIT_FAILURE);                                                                    \
+        }                                                                                          \
+    } while (0)
+
+#define BUFFER_SIZE 4096
+
+int main(int argc, char **argv)
+{
+    check_error(3 == argc, "./a.out destPath srcPath");
+
+    int fdSrc = open(argv[2], O_RDONLY);
+    check_error(fdSrc != -1, "open");
+
+    /* Dohvatanje informacija o izvornom fajlu (npr. privilegija) */
+    struct stat fileinfo;
+    check_error(stat(argv[2], &fileinfo) != -1, "stat");
+
+    /**
+     * Otvaranje (ili kreiranje) destinacionog fajla.
+     * Privilegije ce kasnije biti podesene pomocu chmod.
+     *
+     */
+    int fdDest = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    check_error(fdDest != -1, "open");
+
+    char *buffer = malloc(BUFFER_SIZE);
+    check_error(buffer != NULL, "malloc");
+
+    int procitano;
+    errno = 0;
+    while ((procitano = read(fdSrc, buffer, BUFFER_SIZE)) > 0) {
+        check_error(write(fdDest, buffer, procitano) == procitano, "write");
+        errno = 0;
+    }
+    check_error(procitano == 0, "read");
+
+    /* Postavljanje istih privilegija kao kod izvornog fajla */
+    check_error(chmod(argv[1], fileinfo.st_mode) != -1, "chmod");
+
+    close(fdSrc);
+    close(fdDest);
+    free(buffer);
+    return 0;
+}
