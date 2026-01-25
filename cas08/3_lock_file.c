c3593e79113e8bca84c68ec4624830c124fbfcca
Filip Vidojevic <96ficke@gmail.com>
2026-01-25 21:57:40 +0100

    Sredjen cas08

--- cas08/3_lock_file.c
+++ cas08/3_lock_file.c
@@ -1,24 +1,21 @@
 /*
-Napisati program koji kao argument komandne linije dobija putanju do regularnog fajla. Program treba da čita
-fajl liniju po liniju. Ukoliko linija nije zaključana, program treba da je upiše u fajl čija se putanja zadaje kao drugi
-argument komandne linije. Na kraju rada program treba da ispiše ukupan broj linija koje su upisane. Linija neće
-biti duža od 255 karaktera.
+Program demonstrira rad sa zakljucavanjem fajlova. Poziv programa:
+./lock_file path/to/file start bytes sleepTimeInSec lockType (r or w)
+
+Potrebno je zakljucati deo fajla za citanje/pisanje na putanji 'path/to/file' 
+pocev od bajta 'start' duzine 'bytes'. Nakon toga uspavati proces
+na 'sleepTimeInSec' sekundi, da bi drugi procesi stigli da probaju 
+sa zakljucavanjem.
 */
-#define _XOPEN_SOURCE 700
+
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <stdbool.h>
+#include <unistd.h>
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-#include <sys/wait.h>
-#include <pthread.h>
-#include <errno.h>
-#include <time.h>
-#include <ftw.h>
+#include <stdbool.h>
 
 #define check_error(cond,msg)\
   do {\
@@ -29,57 +26,62 @@ biti duža od 255 karaktera.
     }\
   } while (0)
 
-#define MAX_SIZE (1024)
+static const char *os_Usage = "Usage: ./lock_file path/to/file start bytes sleepTimeInSec lockType (r or w)";
 
-int main(int argc, char** argv) {
-
-    check_error(argc == 3, "...");
-    //otvaramo fajl za citanje i pisanje
-    int fd = open(argv[1], O_RDWR);
-    check_error(fd != -1, "...");
-
-    FILE* f = fdopen(fd, "r+");
-    check_error(f != NULL, "...");
-
-    FILE *output = fopen(argv[2], "w");
-    check_error(output != NULL, "fopen");
-
-    char buffer[MAX_SIZE];
-    int numLines = 0;
-    //citamo liniju po liniju u fajlu
-    while (fgets(buffer, MAX_SIZE, f) != NULL) {
-        //struktura koja opisuje katanac
-        struct flock lock;
-        lock.l_type = F_WRLCK;
-        lock.l_start = ftell(f);
-        lock.l_len = -strlen(buffer);
-        lock.l_whence = SEEK_SET;
-        //postavljamo katanac
-        int retVal = fcntl(fd, F_SETLK, &lock);
-
-        //ukoliko je uspesno postavljen katanac
-        if (retVal != -1) {
-            fprintf(output, "%s", buffer);
-            //povecavamo broj linija
-            numLines++;
-            //otkljucavamo zakljucanu liniju
-            lock.l_type = F_UNLCK;
-            check_error(fcntl(fd, F_SETLK, &lock) != -1, "....");
-        }
-        else {
-            if (errno != EACCES && errno != EAGAIN) {
-                
-                check_error(0, "...");
-            }
-            //ukoliko nije postavljen katanac jer neko drugi ima katanac, idemo dalje
-            continue;
-        }
-    }
-
-    printf("%d\n", numLines);
-
-    fclose(f);
-    fclose(output);
-
-    exit(EXIT_SUCCESS);
+int main(int argc, char **argv) {
+    check_error(6 == argc, os_Usage);
+    
+    /*
+     * parsiramo argumente i pretvaramo ih u odgovarajuce tipove
+     */
+    char *fpath = argv[1];
+    int start = atoi(argv[2]);
+    int length = atoi(argv[3]);
+    int sleepTimeInSec = atoi(argv[4]);
+    int lockType = argv[5][0] == 'r' ? F_RDLCK : F_WRLCK;
+    
+    /*
+     * otvaramo fajl u kombinovanom modu, jer korisnik moze da postawi i r i w katanac.
+     */
+    int fd = open(fpath, O_RDWR);
+    check_error(-1 != fd, "Opening file failed");
+  
+    /* u strukturi flock moramo da navedemo svojstva katanca koji zelimo da postavimo */
+    struct flock lock;
+    lock.l_type = lockType;
+    lock.l_whence = SEEK_SET;
+    lock.l_start = start;
+    lock.l_len = length;
+    printf("Trying to lock...\n");
+    /* pokusavamo da zakljucamo fajl na zeljeni nacin
+    * F_SETLK - ako ne moze da zakljuca fajl, odmah puca i vraca se nazad
+    * F_SETLKW - ako ne moze da zakljuca fajl, sacekace dok se resurs ne oslobodi
+    * F_GETLK - se koristi za ispitivanje da li moze da se postavi zeljeni katanac
+    *           u ovom slucaju nije lose biti pohlepan i uvek ispitivati maksimalne mogucnosti, tj.
+    *           treba postaviti tip katanca na F_WRLCK. Ako fajl moze da se zakljuca u w modu
+    *           sigurno moze i u r modu. Vodite se parolom "ko moze vise moze i manje".
+    *
+    * Kada se vrsi zakljucavanje, uvek treba zakljucavati minimum potrebnih bajtova da bi se omogucio sto
+    * veci paralelizam. 
+    */
+    check_error(-1 != fcntl(fd, F_SETLK, &lock), "Locking file failed");
+    printf("Lock acquired...\n");
+    
+    /*
+     * Uspavljujemo proces cime simuliramo drzanje katanca
+     */
+    sleep(sleepTimeInSec);
+    /* svaki postavljeni katanac je neophodno otkljucati u nekom trenutku 
+    * tip postavljamo na F_UNLCK
+    */
+    lock.l_type = F_UNLCK;
+    /* I otkljucavamo fajl */
+    check_error(-1 != fcntl(fd, F_SETLK, &lock), "Unlocking file failed");
+    printf("Lock released...\n");
+    /* zatvaramo fajl deskriptor
+    * i da zaboravite otkljucavanje, zatvaranjem deskriptora sa automatski oslobadaju svi katanci 
+    */
+    close(fd);
+  
+    return 0;
 }
